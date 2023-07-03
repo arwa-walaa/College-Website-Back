a3590bb674bc84493c4adef407586f83963b7349
sarahessamkamal <82125455+sarahessamkamal@users.noreply.github.com>
2023-07-03 19:53:25 +0300

    announcement when a new course added (done)

--- resources/views/emails/NewAnnouncementNotification.blade.php
+++ resources/views/emails/NewAnnouncementNotification.blade.php
@@ -1,15 +1,23 @@
-@component('mail::message')
+{{ $announcment }}
 
-{{$announcment}}
-
-@if (strpos($announcment, 'new message') === 0)
-    @component('mail::button', ['url' => 'http://localhost:4200/FCAIChat'])
-        View Message
-    @endcomponent
-@else
-    @component('mail::button', ['url' => 'http://localhost:4200/Announcements'])
-        View Announcement
-    @endcomponent
-@endif
-
+@if (strpos($announcment, 'new message') !== false)
+    
+<a href="http://localhost:4200/FCAIChat" class="button">Go to Chat</a>
+@component('mail::button', ['url' => 'http://localhost:4200/FCAIChat'])
+View Message
 @endcomponent
+
+@elseif (strpos($announcment, 'You have been assigned to a new course ') !== false)
+    
+<a href="http://localhost:4200/dr_ta_courses" class="button">View My Courses</a>
+@component('mail::button', ['url' => 'http://localhost:4200/dr_ta_courses'])
+View My Courses
+@endcomponent
+        
+@else       
+@component('mail::button', ['url' => 'http://localhost:4200/Announcements'])
+View Announcement
+@endcomponent
+
+
+@endif
